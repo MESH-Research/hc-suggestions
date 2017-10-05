3fd7b76f6b4ae0479a55bb277417f110d543abb2
Ryan Williams <rwilliams@mla.org>
2017-10-05 11:47:53 -0400

    first

---
+++ hc-suggestions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     HC Suggestions
+ * Plugin URI:      https://github.com/mlaa/hc-suggestions
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          MLA
+ * Author URI:      https://github.com/mlaa
+ * Text Domain:     hc-suggestions
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Hc_Suggestions
+ */
