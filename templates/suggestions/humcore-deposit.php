b945dfd7680b7c17fb62b0dfa96db88042c15ab9
Ryan Williams <rwilliams@mla.org>
2018-03-28 20:46:50 +0000

    use @ prefix when working with potentially nonexistent deposit metadata to avoid php notices

--- templates/suggestions/humcore-deposit.php
+++ templates/suggestions/humcore-deposit.php
@@ -9,23 +9,23 @@ global $post;
 
 // CORE deposit icons & download URLs depend on file data set in post meta.
 $post_meta = $post->post_meta;
-$file_metadata = json_decode( $post_meta['_deposit_file_metadata'][0], true );
+$file_metadata = @json_decode( $post_meta['_deposit_file_metadata'][0], true );
 
 // CORE icon.
-$file_type_data = wp_check_filetype( $file_metadata['files'][0]['filename'], wp_get_mime_types() );
+$file_type_data = @wp_check_filetype( $file_metadata['files'][0]['filename'], wp_get_mime_types() );
 if ( empty( $file_type_data['ext'] ) ) {
 	$file_type_data = [
 		'ext' => 'txt',
 	]; // Fallback in case we didn't find an actual type.
 }
-$avatar_img = sprintf(
+$avatar_img = @sprintf(
 	'<img class="deposit-icon" src="%s" alt="%s" />',
 	'/app/plugins/humcore/assets/' . esc_attr( $file_type_data['ext'] ) . '-icon-48x48.png',
 	esc_attr( $file_type_data['ext'] )
 );
 
 // CORE download URL.
-$download_url = sprintf(
+$download_url = @sprintf(
 	'/deposits/download/%s/%s/%s/',
 	$file_metadata['files'][0]['pid'],
 	$file_metadata['files'][0]['datastream_id'],
