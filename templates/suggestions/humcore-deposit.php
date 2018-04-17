729af8142a93dc2440e001bba5b03c7d13d13450
Ryan Williams <rwilliams@mla.org>
2018-04-17 17:24:10 -0400

    ignore phpcs warnings in humcore-deposit template

--- templates/suggestions/humcore-deposit.php
+++ templates/suggestions/humcore-deposit.php
@@ -8,16 +8,19 @@
 global $post;
 
 // CORE deposit icons & download URLs depend on file data set in post meta.
-$post_meta     = $post->post_meta;
+$post_meta = $post->post_meta;
+// @codingStandardsIgnoreLine
 $file_metadata = @json_decode( $post_meta['_deposit_file_metadata'][0], true );
 
 // CORE icon.
+// @codingStandardsIgnoreLine
 $file_type_data = @wp_check_filetype( $file_metadata['files'][0]['filename'], wp_get_mime_types() );
 if ( empty( $file_type_data['ext'] ) ) {
 	$file_type_data = [
 		'ext' => 'txt',
 	]; // Fallback in case we didn't find an actual type.
 }
+// @codingStandardsIgnoreLine
 $avatar_img = @sprintf(
 	'<img class="deposit-icon" src="%s" alt="%s" />',
 	'/app/plugins/humcore/assets/' . esc_attr( $file_type_data['ext'] ) . '-icon-48x48.png',
@@ -25,6 +28,7 @@ $avatar_img = @sprintf(
 );
 
 // CORE download URL.
+// @codingStandardsIgnoreLine
 $download_url = @sprintf(
 	'/deposits/download/%s/%s/%s/',
 	$file_metadata['files'][0]['pid'],
